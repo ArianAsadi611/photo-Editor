2b8c48b54d47061f777839f394a38449086bf930
Arian Asadi <arianasadi611@gmail.com>
2022-09-10 23:59:24 +0430

    Selected elements

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Selected Buttons and inputs.
+const fileInput = document.querySelector(".file-input"),
+  progressInput = document.querySelector("input[type = 'range']"),
+  filterOption = document.querySelectorAll(".right li"),
+  quickAccess = document.querySelectorAll(".left li");
+
+// Selected Progress information.
+let filterName = document.querySelector(".filter-info .name");
+let filterValue = document.querySelector(".filter-info .value");
