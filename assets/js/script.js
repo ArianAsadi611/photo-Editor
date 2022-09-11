2dd2436feb75ac603298510de73cc7615d557cc1
Arian Asadi <arianasadi611@gmail.com>
2022-09-11 23:08:35 +0430

    A function was written to upload the photo and the filter property values were specified
    A photo element has also been added.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,32 @@ const fileInput = document.querySelector(".file-input"),
 // Selected Progress information.
 let filterName = document.querySelector(".filter-info .name");
 let filterValue = document.querySelector(".filter-info .value");
+let previewImage = document.querySelector(".preview-img img");
+
+// Values were set for filter properties
+let brightness = "100",
+  saturation = "100",
+  inversion = "0",
+  grayScale = "0";
+
+// This is a function for uploading photos
+const loadImage = () => {
+  let file = fileInput.files[0];
+  console.log(file);
+
+  if (!file) return;
+
+  previewImage.src = URL.createObjectURL(file);
+
+  previewImage.addEventListener("load", () => {
+    quickAccess[2].click();
+    document.querySelector(".right").classList.remove("disable");
+    document.querySelector("footer").classList.remove("disable");
+    document.querySelector(".preview-img").classList.remove("disable");
+  });
+};
+
+// click + button
+quickAccess[0].addEventListener("click", () => fileInput.click());
+//--
+fileInput.addEventListener("change", loadImage);
