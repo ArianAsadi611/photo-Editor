15fbce28ab2ff2101167da94e264f74d4f6eade8
Arian Asadi <arianasadi611@gmail.com>
2022-09-12 23:48:28 +0430

    Configuring Buttons Functions are written to apply filters and identify buttons.

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,6 @@ let brightness = "100",
 // This is a function for uploading photos
 const loadImage = () => {
   let file = fileInput.files[0];
-  console.log(file);
 
   if (!file) return;
 
@@ -31,6 +30,50 @@ const loadImage = () => {
     document.querySelector(".preview-img").classList.remove("disable");
   });
 };
+
+// The given value is applied
+const applyFilter = () => {
+  previewImage.style.filter = `brightness(${brightness}%) saturation(${saturation}%) invert(${inversion}%) grayscale(${grayScale})`;
+};
+
+// Control buttons are recognized and activated by clicking and their information is displayed
+filterOption.forEach((option) => {
+  option.addEventListener("click", () => {
+    document.querySelector(".active").classList.remove("active");
+
+    option.classList.add("active");
+
+    switch (option.id) {
+      case "brightness": {
+        progressInput.max = "200";
+        progressInput.value = brightness;
+        filterValue.innerText = `${brightness}%`;
+        break;
+      }
+
+      case "saturation": {
+        progressInput.max = "200";
+        progressInput.value = saturation;
+        filterValue.innerText = `${saturation}%`;
+        break;
+      }
+
+      case "inversion": {
+        progressInput.max = "100";
+        progressInput.value = inversion;
+        filterValue.innerText = `${inversion}%`;
+        break;
+      }
+
+      case "grayScale": {
+        progressInput.max = "100";
+        progressInput.value = grayScale;
+        filterValue.innerText = `${grayScale}%`;
+        break;
+      }
+    }
+  });
+});
 
 // click + button
 quickAccess[0].addEventListener("click", () => fileInput.click());
